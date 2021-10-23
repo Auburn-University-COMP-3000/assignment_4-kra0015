c47150bf39bc878adfa883058a4df7053996753c
Kaitlin Adams <KRA0015@auburn.edu>
2021-10-22 22:24:25 -0500

    Upload of question 1

--- q1.cpp
+++ q1.cpp
@@ -7,22 +7,26 @@ class GasPump {
 	private:
 		double amountDispensed,
 		       amountCharged,
-		       costPerGallon;
+		       costPerGallon,
+			   dispenseTime;
 
 	public:
-		double getAmountDispensed() { return amountDispensed; }
-		double getAmountCharged() { return amountCharged; }
-		double getCostPerGallon() { return costPerGallon; }
-		void setCostPerGallon(double cpg) { costPerGallon = cpg; }
-        
-		void dispense(int seconds) {
-
-
-			
-			amountCharged = amountDispensed * costPerGallon;
+		double getAmountDispensed() { 
+			amountDispensed = 0.10 * dispenseTime;
+			return amountDispensed; 
+		}
+		double getCostPerGallon() { 
+			return costPerGallon; 
+		}
+		double getAmountCharged() { 
+			amountCharged = getCostPerGallon() * getAmountDispensed();
+			return amountCharged; 
+		}
+		void setCostAndTime(double cpg, double time) { 
+			costPerGallon = cpg; 
+			dispenseTime = time;
 		}
 		void reset() {
-            
 			amountDispensed = 0.0;
 			amountCharged = 0.0;
 		}		
@@ -31,11 +35,35 @@ class GasPump {
 int main() {
 	
     //Define variables
-
-    // Define Class Object
+	GasPump gas;
+	char dispenseAgain;
+	double cpg;
+	double seconds;
+	
 
     // Call Functions
-	
+	do {
+		cout << "Enter the cost of gas per gallon: $";
+		cin >> cpg;
+		cout << "Enter the amount of seconds for which you would like to pump gas: ";
+		cin >> seconds;
+		gas.setCostAndTime(cpg, seconds);
+		
+
+		cout << "\nThe cost per gallon of gas is $" << fixed << setprecision(2) << gas.getCostPerGallon() << "." <<endl;
+		cout << "So, the total amount charged for " << fixed << setprecision(2) << gas.getAmountDispensed() << 
+				" gallons of gas dispensed is $" << fixed << setprecision(2) << gas.getAmountCharged() << "." << endl;
+
+		cout << "\nWould you like to dispense gas again? Enter Y for yes or any other key for no: ";
+		cin >> dispenseAgain;
+
+		if (dispenseAgain == 'Y') {
+			gas.reset();
+		}
+	}
+	while(dispenseAgain == 'Y');
+
+
 
 	return 0;
 }
