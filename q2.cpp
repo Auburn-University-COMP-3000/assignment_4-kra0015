4ad706cee8dbf45411ff9f7625fe0b6194503d64
Kaitlin Adams <KRA0015@auburn.edu>
2021-10-22 23:33:30 -0500

    Upload of q2

--- q2.cpp
+++ q2.cpp
@@ -8,14 +8,27 @@ class Temperature {
 		       degreesFahrenheit,
 		       degreesCelsius;
 	public:
-		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
-		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
-		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+		void setTempKelvin(double degreesKelvin) { this->degreesKelvin = degreesKelvin; }
+		double getTempKelvin() { return degreesKelvin; }
+		void setTempCelsius() { this->degreesCelsius = degreesKelvin - 273.15; }
+		double getTempCelsius() { return degreesCelsius; } 
+		void setTempFahrenheit() { this->degreesFahrenheit = (degreesCelsius)*(9/5.0) + 32; }
+		double getTempFahrenheit() { return degreesFahrenheit; }
 };
 
 int main() {
+	Temperature temps;
+	double tempK;
 
+	cout << "Enter a temperature in degrees Kelvin: ";
+	cin >> tempK;
+	temps.setTempKelvin(tempK);
+	temps.setTempCelsius();
+	temps.setTempFahrenheit();
+
+	cout << "\n" << temps.getTempKelvin() << " degrees Kelvin is equal to: " << endl <<
+			"\t" << temps.getTempCelsius() << " degrees Celsius" << endl <<
+			"\t" << temps.getTempFahrenheit() << " degrees Fahrenheit" << endl;
+
+	return 0;
 }
